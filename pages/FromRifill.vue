cb040d2e2f0b3c84823a1f376115c50681ff09b1
peepo325 <68525284+peepo325@users.noreply.github.com>
2020-10-08 13:50:12 +0700

    edit

---
+++ pages/FromRifill.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    <v-row>
+      <v-col cols="3">
+        <img
+          src="https://www.lnwtrue.com/static/images/product/truemoney-1.jpg"
+          alt=""
+        />
+      </v-col>
+      <v-col cols="3">
+        <img
+          src="https://www.lnwtrue.com/static/images/product/12call-1.jpg"
+          alt=""
+        />
+      </v-col>
+      <v-col cols="3">
+        <img
+          src="http://tasty-dsign.com/demo/playzone/images/dtac-1.jpg"
+          alt=""
+        />
+      </v-col>
+    </v-row>
+    <v-card class="mx-auto pa-4" elevation="11">
+      <h3>ซื้อบัตรเติมเงินโทรศัพท์</h3>
+      <v-form ref="form">
+        <v-row>
+          <v-col cols="8">
+            <v-text-field
+              v-model="phone"
+              label="เบอร์โทรศัพท์"
+              prepend-icon="mdi-cellphone"
+              required
+            ></v-text-field>
+          </v-col>
+          <v-col cols="4">
+            <v-select
+              v-model="genre"
+              :items="genre"
+              label="เครือข่าย"
+              required
+            ></v-select>
+          </v-col>
+        </v-row>
+        <v-row>
+          ราคา
+          <v-checkbox
+            v-model="price"
+            label="50"
+            value="50"
+            hide-details="false"
+          >
+          </v-checkbox>
+          <v-checkbox v-model="price" label="90" value="90"> </v-checkbox>
+          <v-checkbox v-model="price" label="150" value="150"> </v-checkbox>
+          <v-checkbox v-model="price" label="300" value="300"> </v-checkbox>
+          <v-checkbox v-model="price" label="500" value="500"> </v-checkbox>
+        </v-row>
+
+        <v-row>
+          <v-col cols="10"> </v-col>
+          <v-col cols="2">
+            <v-btn color="primary" text @click="set()"> Submit </v-btn>
+          </v-col>
+        </v-row>
+      </v-form>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      phone: '',
+      genre: ['AIS', 'Dtac', 'Truemove', 'TOT Moblie'],
+      price: [],
+      dialog1: false,
+    }
+  },
+  methods: {
+    dialog(val) {
+      if (!val) return
+
+      setTimeout(() => (this.dialog = false), 4000)
+    },
+    set() {
+      this.arr = {
+        genre: this.genre,
+        phone: this.phone,
+        price: this.price,
+      }
+      this.$store.commit('set_arr', this.arr)
+    },
+  },
+}
+</script>
---
+++ pages/Refill.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <v-card>
+      <v-card-title>
+        <v-spacer></v-spacer>
+      </v-card-title>
+      <v-data-table
+        :headers="headers"
+        :items="listdata"
+        :items-per-page="5"
+        :search="search"
+        class="elevation-1"
+      ></v-data-table>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      search: '',
+      headers: [
+        {
+          text: 'phone',
+          align: 'start',
+          sortable: false,
+          value: 'phone',
+        },
+        {
+          text: 'genre',
+          value: 'genre',
+        },
+        {
+          text: 'price',
+          value: 'price',
+        },
+      ],
+    }
+  },
+  computed: {
+    listdata: {
+      get() {
+        return this.$nuxt.$store.state.list
+      },
+    },
+  },
+}
+</script>
